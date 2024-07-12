1839f08027ca25d9b9072454e93ed24130cd57c0
Boghdady9 <MohamedBoghdady@my.uopeople.edu>
2024-07-12 19:28:08 +0300

    Update Buy&sell_stock.py

--- array&strring/Buy&sell_stock.py
+++ array&strring/Buy&sell_stock.py
@@ -1,14 +1,14 @@
-n=[7,1,5,3,6,4]
-start=0
-end=0
-profit=0
-while end <= len(n)-1 :
-    if n[end] >= n[start]:
-        profit=max(profit,n[end]-n[start])
-        end+=1
+prices=[7,1,5,3,6,4]
+start = 0
+end = 0
+profit = 0
+
+while end < len(prices):
+    if prices[end] >= prices[start]:
+        profit = max(profit, prices[end] - prices[start])
     else:
-       start+=end
-       end+=1
+        start = end
+    end += 1
 
 print(profit)
 
